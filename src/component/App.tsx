429ab7145d7a38ced0c060f5248a42594b18791c
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-18 16:20:06 +0100

    css ok

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,9 +1,15 @@
-import {Routing} from "./Routing";
-import {HomeHero} from "./Home/HomeHero";
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+import {HomeHero} from "./Home/HomeHero";
 
 export const App = () => {
     return (
-        <HomeHero />
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomeHero />} />
+                <Route path="/practice" element={<HomeHero />} />
+            </Routes>
+        </Router>
     )
 };
