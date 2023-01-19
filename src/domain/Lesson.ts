6df2496850f43b86dfc052981c1f037bb5913177
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-19 14:40:54 +0100

    withId test

--- src/domain/Lesson.ts
+++ src/domain/Lesson.ts
@@ -6,8 +6,8 @@ export class Lesson {
     title:string;
     order: number;
 
-    constructor(id:string, syllables:Syllable[], title:string, order:number) {
-        this.id = id;
+    constructor(syllables:Syllable[], title:string, order:number) {
+        this.id = "";
         this.syllables = syllables;
         this.title = title;
         this.order = order;
@@ -15,17 +15,19 @@ export class Lesson {
 
 }
 
+export const withId = (lesson:Lesson) => ({...lesson, id: lesson.title.split(" ").join("")});
+
 const syllablesLesson1 = [
     new Syllable('hiragana', '', 'a'),
     new Syllable('hiragana', '', 'i'),
 ];
-const lesson1 = new Lesson('aiueo', syllablesLesson1, 'a i u e o', 1);
+const lesson1 = withId(new Lesson(syllablesLesson1, 'a i u e o', 1));
 
 const syllablesLesson2 = [
     new Syllable('hiragana', 'k', 'ka'),
     new Syllable('hiragana', 'k', 'ki'),
 ];
-const lesson2 = new Lesson('kakikukeko', syllablesLesson1, 'ka ki ku ke ko', 1);
+const lesson2 =  withId(new Lesson(syllablesLesson1, 'ka ki ku ke ko', 1));
 
 export function lessonCatalog():Array<Lesson> {
     let lessons:Array<Lesson> = [];
--- src/test/Lesson.test.ts
+++ src/test/Lesson.test.ts
@@ -1,12 +1,30 @@
-import {Lesson, lessonCatalog} from "../domain/Lesson";
+import {Lesson, lessonCatalog, withId} from "../domain/Lesson";
+import {Syllable} from "../domain/Syllable";
 
+const syllables = [
+    new Syllable('hiragana', 'k', 'ka'),
+    new Syllable('hiragana', 'k', 'ki'),
+    new Syllable('hiragana', 'k', 'ku'),
+    new Syllable('hiragana', 'k', 'ke'),
+    new Syllable('hiragana', 'k', 'ko'),
+];
+const lesson:Lesson = new Lesson(syllables, 'ka ki ku ke ko', 1);
 const lessons:Array<Lesson> = lessonCatalog();
 
 describe('Lesson', function () {
+
+    it(`should provide lesson with id`, function () {
+        const sut = withId(lesson).id;
+        const expected = 'kakikukeko';
+
+        expect(sut).toBe(expected)
+    });
+
     it(`should provide lessons structured in alphabets`, function () {
         const sut = lessons;
         const expected = 2;
 
         expect(sut.length).toBe(expected)
     });
+
 });
