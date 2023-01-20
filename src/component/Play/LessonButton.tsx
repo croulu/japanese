5a4350444adb2e503b0ce55ca5f31de22e97c4fb
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-20 18:48:42 +0100

    alphabets et ménage components (oubli fichier)

---
+++ src/component/Play/LessonButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import {Lesson} from "../../domain/Lesson";
+import {SyllableKana} from "../../domain/SyllableKana";
+
+export const LessonButton = (props: { lesson: Lesson }) => {
+    const { lesson } = props;
+    const oneKanaPerLesson = new SyllableKana(lesson.syllables[0].alphabet, lesson.syllables[0].consonant, lesson.syllables[0].vowel)
+    return (
+        <>
+            {oneKanaPerLesson.display()}
+        </>
+    );
+};
