dde9ef2ade682aafd46bec79adf169e09a05a4fa
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-24 07:33:38 +0100

    remove warning

--- src/component/PlayItem/Proposal.tsx
+++ src/component/PlayItem/Proposal.tsx
@@ -7,7 +7,7 @@ import {SyllableKana} from "../../domain/SyllableKana";
 export const Proposal = (props: { syllable:Syllable, isKanaToGuess:Boolean }) => {
     const { syllable, isKanaToGuess } = props;
 
-    let displayProposal:string = "";
+    let displayProposal:string;
     if (isKanaToGuess) {
         const syllableRomaji = new SyllableRomaji(syllable.alphabet, syllable.consonant, syllable.vowel);
         displayProposal = syllableRomaji.display();
