3735a8ad385db28e523d4a26bb421187bbc3a64f
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-02-02 11:06:55 +0100

    remove dead code

--- src/domain/Alphabet.ts
+++ src/domain/Alphabet.ts
@@ -7,19 +7,3 @@ export class Alphabet {
     }
 
 }
-
-/*
-const lessons:Array<Lesson> = groupSyllablesInLessons();
-let kanas:Array<string> = [];
-let romajis:Array<string> = [];
-
-lessons.forEach(lesson =>
-    lesson.syllables.forEach(
-        syllable => {
-            //console.log(syllable.alphabet);
-            const syllableKana = new SyllableKana(syllable.alphabet, syllable.consonant, syllable.vowel)
-            const syllableRomaji = new SyllableRomaji(syllable.alphabet, syllable.consonant, syllable.vowel)
-            kanas.push(syllableKana.display());
-            romajis.push(syllableRomaji.display());
-        }));
-        */
